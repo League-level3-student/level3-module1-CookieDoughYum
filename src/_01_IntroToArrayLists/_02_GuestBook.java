c0fb4fe2272a02af734be9180507476b5efc781a
root <root@3891c8493c3c>
2020-04-06 23:59:46 +0000

    Working on guest book.

--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,10 +1,15 @@
 package _01_IntroToArrayLists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class _02_GuestBook {
+public class _02_GuestBook implements ActionListener{
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -13,18 +18,40 @@ public class _02_GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	JButton button=new JButton("Add Name");
+	JButton button1=new JButton("View Names");
+    ArrayList<String> names=new ArrayList<String>();
 	public static void main(String[] args) {
-		
+	    _02_GuestBook gb=new _02_GuestBook();
+		gb.GUI();
 	}
 	void GUI() {
 		JFrame frame=new JFrame();
+		frame.setVisible(true);
 		JPanel panel=new JPanel();
 		frame.add(panel);
-		JButton button=new JButton("Add Name");
 		panel.add(button);
-		JButton button1=new JButton("View Names");
+		button.addActionListener(this);
 		panel.add(button1);
+		button1.addActionListener(this);
 		frame.pack();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==button) {
+			String name=JOptionPane.showInputDialog("Please enter a name to add to the guest book");
+			names.add(name);
+		}
+		if(e.getSource()==button1) {
+			String book="";
+			for(int i=0; i<names.size(); i++) {
+				//System.out.println(i);
+				book+=("Guest #" + i+1 + ":");
+				book+=(names.get(i));
+			}
+			JOptionPane.showMessageDialog(null, book);
+		}
 	}
 	
 	
