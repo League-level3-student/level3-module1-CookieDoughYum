19a693ef369e701c83103ceb05833cbb4eb5ea6f
root <root@d7cecf4a298b>
2020-05-11 23:55:21 +0000

    Working on hangman - step 2

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,17 +1,45 @@
 package _04_HangMan;
 
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class HangMan{
+	Stack<String> Names=new Stack();
+	String underScore="";
+	JFrame frame=new JFrame();
+	JPanel panel=new JPanel();
+	JLabel label=new JLabel();
+	int lives=6;
 public static void main(String[] args) {
 	HangMan hm=new HangMan();
-  hm.startGame();
-  Utilities.readRandomLineFromFile("dictionary.txt");
+    hm.startGame();
+    hm.GUI();
 }	
 
 void startGame() {
-	JOptionPane.showInputDialog("Pick a number that is up to 266" );
+	String number=JOptionPane.showInputDialog("Pick a number that is up to 266" );
+	 int num=Integer.parseInt(number);
+	for(int i=0; i<num; i++) {
+		  Names.push(Utilities.readRandomLineFromFile("dictionary.txt"));
+		  }
+	String playWord=Names.pop();
+	char[] a=playWord.toCharArray();
+	for(int i=0; i<a.length; i++) {
+		underScore+="__ ";
+		String guess=JOptionPane.showInputDialog(" What is your guess for character number " + i + " ? ")
+	}
 }
-	
+
+void GUI() {
+	frame.setVisible(true);
+	frame.add(panel);
+	panel.add(label);
+	frame.pack();
+	label.setText(underScore);
+}
 	
 }
