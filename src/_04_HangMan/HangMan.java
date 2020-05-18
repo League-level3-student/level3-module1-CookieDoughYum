367061f237db2f169d9d558e9b5436bd1ac5581d
root <root@7caf807a6ca3>
2020-05-18 23:56:00 +0000

    Working on Hangman - step 4

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,5 +1,7 @@
 package _04_HangMan;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -7,9 +9,11 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class HangMan{
+public class HangMan implements KeyListener{
 	Stack<String> Names=new Stack();
 	String underScore="";
+	String playWord;
+	char[] a;
 	JFrame frame=new JFrame();
 	JPanel panel=new JPanel();
 	JLabel label=new JLabel();
@@ -23,23 +27,67 @@ public static void main(String[] args) {
 void startGame() {
 	String number=JOptionPane.showInputDialog("Pick a number that is up to 266" );
 	 int num=Integer.parseInt(number);
-	for(int i=0; i<num; i++) {
+	 for(int i=0; i<num; i++) {
 		  Names.push(Utilities.readRandomLineFromFile("dictionary.txt"));
 		  }
-	String playWord=Names.pop();
-	char[] a=playWord.toCharArray();
+	 restart();
+}
+
+void restart() {
+	playWord=Names.pop();
+	a=playWord.toCharArray();
+	underScore="";
 	for(int i=0; i<a.length; i++) {
 		underScore+="__ ";
-		String guess=JOptionPane.showInputDialog(" What is your guess for character number " + i + " ? ")
 	}
-}
+	}//
+
+
+
 
 void GUI() {
 	frame.setVisible(true);
 	frame.add(panel);
+	frame.addKeyListener(this);
 	panel.add(label);
 	frame.pack();
 	label.setText(underScore);
+}
+
+@Override
+public void keyPressed(KeyEvent e) {
+	StringBuilder sb=new StringBuilder(underScore);
+	// TODO Auto-generated method stub
+	for(int i=0; i<a.length; i++) {
+		if(e.getKeyChar()==a[i]) {
+			sb.replace(i*3, i*3+2, e.getKeyChar()+" ");
+		}
+		else {
+			lives-=1;
+		}
+	}
+	underScore=sb.toString();
+	if(!underScore.contains("_")) {
+		label.setText("");
+		restart();
+		lives=6;
+	}
+	
+	label.setText(underScore);
+	
+	
+}
+
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
 }
 	
 }
