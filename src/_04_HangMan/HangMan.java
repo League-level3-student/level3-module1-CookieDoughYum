e6d173a80acc8a75ab2deb88e761b33ef2d69d74
root <root@0a76b52db9aa>
2020-06-11 03:06:58 +0000

    Working on HangMan.

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -11,14 +11,14 @@ import javax.swing.JPanel;
 
 public class HangMan implements KeyListener{
 	Stack<String> Names=new Stack();
-	String underScore="";
-	String playWord;
-	char[] a;
-	JFrame frame=new JFrame();
-	JPanel panel=new JPanel();
-	JLabel label=new JLabel();
+	static String underScore="";
+	static String playWord="";
+	//char[] a;
+	static JFrame frame=new JFrame();
+	static JPanel panel=new JPanel();
+	static JLabel label=new JLabel();
 	int lives=6;
-	StringBuilder sb=new StringBuilder();
+	//static String sb="";
 public static void main(String[] args) {
 	HangMan hm=new HangMan();
     hm.startGame();
@@ -34,33 +34,21 @@ void startGame() {
 	 lives=6;
 	 playWord=Names.pop();
 	 for(int i=0; i<playWord.length(); i++) {
-			underScore+="__ ";
+			underScore+="_";
 		}
 	 label.setText(underScore);
-	 a=playWord.toCharArray();
+	 //a=playWord.toCharArray();
 	 System.out.println(playWord);
 }
 
-void restart() {
-	//System.out.println("re");
-	if(Names.isEmpty()==false) {
-		playWord=Names.pop();
-		sb=new StringBuilder(playWord.length());
-		//System.out.println(playWord + "playWord");
-	a=playWord.toCharArray();
-	underScore="";
-	System.out.println(a);
-	for(int i=0; i<playWord.length(); i++) {
-		underScore+="__ ";
-	}
-	label.setText(underScore);
-	lives=6;
-	}
-	else {
-		JOptionPane.showMessageDialog(null, "There are no words in the list");
-	}
-	}//
-
+/*
+ * void restart() { //System.out.println("re"); if(Names.isEmpty()==false) {
+ * playWord=Names.pop(); sb=playWord; //System.out.println(playWord +
+ * "playWord"); a=playWord.toCharArray(); underScore=""; System.out.println(a);
+ * for(int i=0; i<playWord.length(); i++) { underScore+="__ "; }
+ * label.setText(underScore); lives=6; } else {
+ * JOptionPane.showMessageDialog(null, "There are no words in the list"); } }//
+ */
 
 
 
@@ -77,44 +65,44 @@ void GUI() {
 public void keyPressed(KeyEvent e) {
 	//System.out.println(lives);
 	// TODO Auto-generated method stub
-	boolean charFound=false;
-	for(int i=0; i<a.length; i++) {
-		if(e.getKeyChar()==a[i]) {
-			charFound=true;
-			sb=sb.replace(i*3, i*3+2, e.getKeyChar()+" ");
-			underScore=sb.toString();
+	char guess=e.getKeyChar();
+	for(int i=0; i<playWord.length(); i++) {
+		if(guess==playWord.charAt(i)) {
+			//charFound=true;
+			underScore=underScore.substring(0, i)+guess+underScore.substring(i+1);
+			//underScore=sb.toString();
 			label.setText(underScore);
 		}
-	}
-	if(!charFound) {
-		lives-=1;
+		else {insert
+			lives-=1;
+		}
 	}
 	if(lives==0) {
 		 JOptionPane.showMessageDialog(null, "Game Over");
-		 String again=JOptionPane.showInputDialog("Would you like to play again?");
-		 if(again.equals("yes")) {
+		 System.exit(0);
 			 //System.out.println("re");
 			 //label.setText(underScore);
-			 System.out.println("Restart");
-			 restart();
-		 }
-		 if(again.equals("no")) {
-			 System.exit(0);
-		 }
+			 //System.out.println("Restart");
 	 }
-	//text=sb.toString();
-	if(!underScore.contains("_")) {
-		//not working
-		System.out.println("restart");
-		restart();
-	}
+	//text=sb.toString()
+    }
 	//label.setText(underScore);
-}
+
 
 @Override
 public void keyReleased(KeyEvent e) {
 	// TODO Auto-generated method stub
-	
+	if(label.getText().equals(playWord)) {
+		lives=6;
+		//not working
+		//System.out.println("restart");
+		playWord=Names.pop();
+		underScore="";
+		for(int i=0; i<playWord.length(); i++) {
+			underScore=underScore + "_";
+		}
+		label.setText(underScore);
+	}
 }
 
 @Override
